7702301997ee73c1b1d32d19c160053774d15190
Edvard <Edvard@KOMPKRASAVCIKA>
2023-04-14 18:28:52 +0400

    Add project files.

---
+++ LetterCounter1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LetterCounter1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Write a word: ");
+                string str = Console.ReadLine();
+
+                while (str.Length > 0)
+                {
+                    Console.Write(str[0] + " = ");
+                    int counter = 0;
+                    for (int j = 0; j < str.Length; j++)
+                    {
+                        if (str[0] == str[j])
+                        {
+                            counter++;
+                        }
+                    }
+                    Console.WriteLine(counter);
+                    str = str.Replace(str[0].ToString(), string.Empty);
+                }
+
+                Console.ReadLine();
+        }
+    }
+}
